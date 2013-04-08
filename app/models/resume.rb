6f8826fb94c12a2520b5c247b8a8c7530d488ff5
Melvin Ram <mr@webdesigncompany.net>
2013-04-08 07:17:50 -0700

    Changed logo

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -10,6 +10,7 @@ class Resume < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :video, VideoUploader
+  mount_uploader :web_video, VideoUploader
 
   accepts_nested_attributes_for :addresses
   accepts_nested_attributes_for :schools
@@ -30,30 +31,35 @@ end
 require 'open-uri'
 
 class VideoWorker
-  WEB_MP4_PRESET_ID = '1351620000000-100070'
+  WEB_MP4_PRESET_ID = '1365404213334-8f776a'
 
   def self.perform(id, key)
-    resume = Resume.find(id)
-    resume.key = key
+    @resume = Resume.find(id)
+    @resume.update_column :web_video, nil
+    @resume.update_column :video, nil
+    # raise
+    @resume.key = key
     # resume.remote_video_url = resume.video.direct_fog_url(with_path: true)
     # resume.save!
+    # raise
+    @web_video_key = "resumes/#{@resume.id}/web_video/#{SecureRandom.hex(3)}-#{File.basename(@resume.key)}"
 
-    resume.update_column 'web_video',
-      File.basename(resume.video.path)
+    @resume.update_column 'web_video', @web_video_key
+
 
     transcoder = AWS::ElasticTranscoder::Client.new
     transcoder.create_job(
       pipeline_id: PIPELINE_ID,
       input: {
-        key: resume.video.path,
+        key: @resume.key,
         frame_rate: 'auto',
-        resolution: '640x360',
+        resolution: 'auto',
         aspect_ratio: 'auto',
         interlaced: 'auto',
         container: 'auto'
       },
       output: {
-        key: resume.web_video,
+        key: "uploads/" + @resume.web_video.path,
         preset_id: WEB_MP4_PRESET_ID,
         thumbnail_pattern: "",
         rotate: '0'
