4a1df3a358b179f7074ff5727ef70db5a39abe3d
Melvin Ram <mr@webdesigncompany.net>
2013-04-13 23:31:31 -0700

    Fixed bug in abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,8 +9,6 @@ class Ability
 
     if user.admin?
       can :manage, :all
-    else
-      can :read, :all
     end
 
     # The first argument to `can` is the action you are giving the user permission to do.
