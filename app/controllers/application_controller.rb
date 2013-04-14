36497c3c70fb3a34dce4fbd5b8faa67af5e7cc83
Melvin Ram <mr@webdesigncompany.net>
2013-04-13 23:48:09 -0700

    Trying to fix error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   end
 
   unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: lambda { |exception| render_error 500, exception; raise exception; }
-    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception; raise exception; }
+    rescue_from Exception, with: lambda { |exception| raise exception; render_error 500, exception; }
+    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception|  raise exception; render_error 404, exception; }
   end
 
   private
