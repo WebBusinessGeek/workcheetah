eef4e6f7dc460a8cfa41b69df8bf4babd0a32d43
Melvin Ram <mr@webdesigncompany.net>
2013-04-15 03:48:54 -0700

    Attempting to AccessDenied error on AWS

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -7,4 +7,8 @@ class VideoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
   process :set_content_type
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
 end
