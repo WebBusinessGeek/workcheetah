b156eee062cabfcc721a77c04001b22128c94b0d
Melvin Ram <mr@webdesigncompany.net>
2013-04-15 03:45:15 -0700

    Fixed a problem with carrierwave initializer.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,6 @@ if Rails.env.production?
     # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.max_file_size     = 120.megabytes
-    config.upload_expiration = 10.hours.from_now.utc
   end
 
 elsif Rails.env.development?
