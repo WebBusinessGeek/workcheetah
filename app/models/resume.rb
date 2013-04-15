092b7e092b9088dc25f5c5777e873dbe6b1cd346
Melvin Ram <mr@webdesigncompany.net>
2013-04-15 00:57:15 -0700

    Recompiled assets and got the video thing working.

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -7,8 +7,8 @@ class Resume < ActiveRecord::Base
   has_many :references, dependent: :destroy
   belongs_to :user
 
-  # mount_uploader :video, VideoUploader
-  # mount_uploader :web_video, VideoUploader
+  mount_uploader :video, VideoUploader
+  mount_uploader :web_video, VideoUploader
 
   accepts_nested_attributes_for :addresses
   accepts_nested_attributes_for :schools
@@ -16,54 +16,73 @@ class Resume < ActiveRecord::Base
   accepts_nested_attributes_for :experiences
   accepts_nested_attributes_for :user
 
-  # after_save :enqueue_video
+  after_save :enqueue_video
 
-  # def video_name
-  #   File.basename(video.path || video.filename) unless video.blank?
-  # end
+  def video_name
+    File.basename(video.path || video.filename) unless video.blank?
+  end
 
-  # def enqueue_video
-  #   VideoWorker.perform(id, key) if key.present?
-  # end
+  def enqueue_video
+    VideoWorker.perform(id, key) if key.present?
+  end
 end
+
+# class VideoWorker
+#   def self.perform(id, key)
+#     raise
+#     @resume = Resume.find(id)
+#     @resume.update_column(:video, key)
+#     @web_video_key = "resumes/#{@resume.id}/web_video/#{SecureRandom.hex(3)}-#{File.basename(@resume.key)}"
+#     response = Zencoder::Job.create({
+#       :input => @resume.video,
+#       :output => {
+#         :width => "480",
+#         :url => "s3://ladderdev/" + @web_video_key,
+#         :notification => "http://www.workcheetah.com/zencoder/response"
+#       }
+#     })
+#     response
+#     raise
+#   end
+# end
 
 # require 'open-uri'
 
-# class VideoWorker
-#   WEB_MP4_PRESET_ID = '1365404213334-8f776a'
+class VideoWorker
+  WEB_MP4_PRESET_ID = '1365404213334-8f776a'
+  GENERIC_360P_ID = "1351620000000-000040"
 
-#   def self.perform(id, key)
-#     @resume = Resume.find(id)
-#     @resume.update_column :web_video, nil
-#     @resume.update_column :video, nil
-#     # raise
-#     @resume.key = key
-#     # resume.remote_video_url = resume.video.direct_fog_url(with_path: true)
-#     # resume.save!
-#     # raise
-#     @web_video_key = "resumes/#{@resume.id}/web_video/#{SecureRandom.hex(3)}-#{File.basename(@resume.key)}"
+  def self.perform(id, key)
+    @resume = Resume.find(id)
+    @resume.update_column :web_video, nil
+    @resume.update_column :video, nil
+    @resume.update_column(:video, key.gsub("uploads/", ""))
+    # raise
+    # @resume.key = key
+    # @resume.remote_video_url = resume.video.direct_fog_url(with_path: true)
+    # @resume.save!
+    # raise
+    @web_video_key = "resumes/#{@resume.id}/web_video/#{SecureRandom.hex(3)}-#{File.basename(key)}"
 
-#     @resume.update_column 'web_video', @web_video_key
+    @resume.update_column 'web_video', @web_video_key
 
-
-#     transcoder = AWS::ElasticTranscoder::Client.new
-#     transcoder.create_job(
-#       pipeline_id: PIPELINE_ID,
-#       input: {
-#         key: @resume.key,
-#         frame_rate: 'auto',
-#         resolution: 'auto',
-#         aspect_ratio: 'auto',
-#         interlaced: 'auto',
-#         container: 'auto'
-#       },
-#       output: {
-#         key: "uploads/" + @resume.web_video.path,
-#         preset_id: WEB_MP4_PRESET_ID,
-#         thumbnail_pattern: "",
-#         rotate: '0'
-#       }
-#     )
-
-#   end
-# end
+    transcoder = AWS::ElasticTranscoder::Client.new
+    response = transcoder.create_job(
+      pipeline_id: PIPELINE_ID,
+      input: {
+        key: @resume.video.path,
+        frame_rate: 'auto',
+        resolution: 'auto',
+        aspect_ratio: 'auto',
+        interlaced: 'auto',
+        container: 'auto'
+      },
+      output: {
+        key: @resume.web_video.path,
+        preset_id: GENERIC_360P_ID,
+        thumbnail_pattern: "",
+        rotate: '0'
+      }
+    )
+  end
+end
