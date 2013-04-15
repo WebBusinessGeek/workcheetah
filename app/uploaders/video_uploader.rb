92ca155f97de42881d543b6ef246e6a0b0b174de
Melvin Ram <mr@webdesigncompany.net>
2013-04-15 01:41:24 -0700

    Fixed bug in carrierwave initializer.

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,9 +4,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   include Sprockets::Helpers::RailsHelper
   include Sprockets::Helpers::IsolatedHelper
 
-  include CarrierWave::MimeTypes
-  process :set_content_type
-
-
+  # include CarrierWave::MimeTypes
+  # process :set_content_type
 
 end
