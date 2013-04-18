081f553d51dc85b458f0e112144184e25cc1db23
Melvin Ram <mr@webdesigncompany.net>
2013-04-18 02:09:11 -0700

    Disabled actual cc processing for now.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,6 +4,8 @@ if Rails.env.development? || Rails.env.test?
 end
 
 if Rails.env.production?
-  Stripe.api_key = ENV['STRIPE_PRIVATE_KEY']
-  STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
+  Stripe.api_key = "sk_test_wPMSw7QQnYZkjWBKvq0hUxFn"
+  STRIPE_PUBLIC_KEY = "pk_test_h80n7D2FsGImlQj8pt8ddBHB"
+  # Stripe.api_key = ENV['STRIPE_PRIVATE_KEY']
+  # STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
 end
