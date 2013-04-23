2758d52ab73e547c3dcfae033e918d2157382a76
Melvin Ram <mr@webdesigncompany.net>
2013-04-23 12:45:51 -0700

    Added Stripe API keys for production.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,8 +4,8 @@ if Rails.env.development? || Rails.env.test?
 end
 
 if Rails.env.production?
-  Stripe.api_key = "sk_test_wPMSw7QQnYZkjWBKvq0hUxFn"
-  STRIPE_PUBLIC_KEY = "pk_test_h80n7D2FsGImlQj8pt8ddBHB"
+  Stripe.api_key = "sk_live_ZCeSy10vat1bFAY8eRMZqXkh"
+  STRIPE_PUBLIC_KEY = "pk_live_RykFRUMTipol5ciwHTk8ZeIK"
   # Stripe.api_key = ENV['STRIPE_PRIVATE_KEY']
   # STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
 end
