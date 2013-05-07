3bc01a44303f63578d08e3a3faa8d0f51ab40c77
Dennis Charles Hackethal <dennis@stagelink.de>
2013-05-08 01:07:10 +0200

    reconfigure smtp settings for mandrill

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.mandrillapp.com',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => ENV['MANDRILL_USERNAME'],
+  :password       => ENV['MANDRILL_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
