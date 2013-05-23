5593422cea65802ad1d6e6b6d9dc49a97f13c7bd
Dennis Charles Hackethal <dennis@stagelink.de>
2013-05-23 22:12:07 +0200

    refactor ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new(account: Account.new) # guest user (not logged in)
 
-    if user.admin?
-      can :manage, :all
-    end
-
     # The first argument to `can` is the action you are giving the user permission to do.
     # If you pass :manage it will apply to every action. Other common actions here are
     # :read, :create, :update and :destroy.
@@ -18,6 +14,12 @@ class Ability
     # The third argument is an optional hash of conditions to further filter the objects.
     # For example, here the user can only update published articles.
     #
+    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    if user.admin?
+      can :manage, :all
+    end
+
     if user.account
       can :manage, Job, account_id: user.account.id
       can :read, JobApplication, applicant_access: { account_id: user.account.id }
@@ -28,23 +30,9 @@ class Ability
     if !user.admin?
       cannot :manage, Tweet
     end
-    # can :manage, VideoChat do |video_chat| video_chat_managable?(video_chat, user) end
-
-    # can :manage, VideoChat { |video_chat| video_chat_belongs_to_user(user, video_chat) }
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 
   def video_chat_managable?(video_chat, user)
     user == video_chat.requester || user == video_chat.recipient || video_chat.new_record?
   end
-
-  # def video_chat_managable?(video_chat, user)
-  #   raise CanCan::AccessDenied if false #video_chat.recipient != user and video_chat.requester != user
-  # end
-
-  # def video_chat_belongs_to_user? user, video_chat
-  #   User.find(user.id).requested_video_chats.where(id: video_chat.id)# || User.find(user.id).received_video_chats(where: id: video_chat.id) || video_chat.new_record?
-  #   # VideoChat.where("requester_id = ? OR recipient_id = ?", user.id, user.id)
-  # end
 end
