caa68a685b66a16e3f440a420d99effe5696d29a
Dennis Charles Hackethal <dennis@stagelink.de>
2013-06-11 00:00:13 +0200

    configure geocoder

---
+++ config/geocoder.rb
@@ -0,0 +1,20 @@
+# config/initializers/geocoder.rb
+Geocoder.configure(
+
+  # geocoding service (see below for supported options):
+  :lookup => :google,
+
+  # to use an API key:
+  :api_key => [ENV['GOOGLE_MAPS_API_KEY'], ENV['GOOGLE_MAPS_CLIENT_ID'], "channel"],
+
+  # geocoding service request timeout, in seconds (default 3):
+  :timeout => 5,
+
+  # set default units to kilometers:
+  # :units => :km,
+
+  # caching (see below for details):
+  :cache => {},
+  :cache_prefix => "..."
+
+)
