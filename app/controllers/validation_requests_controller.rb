2b6cdc3126be3ca6db10a1471539808cafcf1d65
Dennis Charles Hackethal <dennis@stagelink.de>
2013-06-16 15:45:52 +0200

    fix bugs with validation request mailer

--- app/controllers/validation_requests_controller.rb
+++ app/controllers/validation_requests_controller.rb
@@ -10,10 +10,9 @@ class ValidationRequestsController < ApplicationController
     @validation_request.account = current_user.account
 
     @validation_request.build_address(params[:validation_request][:address])
-    
     if @validation_request.save
       ValidationRequestMailer.delay.new_validation_request(@validation_request)
-      ValidationRequestMailer.delay.validation_receipt(@validation_request, current_user.email)
+      ValidationRequestMailer.delay.validation_receipt(@validation_request, current_user)
       redirect_to [:account], notice: "Thank you for your application. We have provided you with a temporary seal until we conclude review of your application."
     else
       render action: :new
