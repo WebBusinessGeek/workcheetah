7a8bdce2d6d58d8224cf403b37c135781d03d315
Adam Robbie <adam.robbie@gmail.com>
2013-07-24 10:04:14 -0400

    Added account cancelling link, trailing white spaces removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :scam_reports, dependent: :destroy
   has_one :resume, dependent: :destroy
   belongs_to :account
-  has_one :advertiser_account
+  has_one :advertiser_account, dependent: :destroy
   has_many :requested_video_chats, class_name: "VideoChat", foreign_key: :requester_id, dependent: :destroy
   has_many :received_video_chats, class_name: "VideoChat", foreign_key: :recipient_id, dependent: :destroy
   has_many :video_chat_messages, foreign_key: :sender_id, dependent: :destroy
