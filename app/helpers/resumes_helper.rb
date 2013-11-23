14f76ee0cbc4cc9ec4a33447602b3808ba946e36
Adam Robbie <adam.robbie@gmail.com>
2013-11-23 13:17:56 -0500

    updated job search, resume show views

--- app/helpers/resumes_helper.rb
+++ app/helpers/resumes_helper.rb
@@ -1,9 +1,9 @@
 module ResumesHelper
   def render_rating_display(rating)
     case rating
-    when 0..30 then @rating_class = "label-important"
-    when 31..50 then @rating_class = "label-warning"
-    when 51..70 then @rating_class = ""
+    when 0..30 then @rating_class = "label-danger"
+    when 31..50 then @rating_class = "label-default"
+    when 51..70 then @rating_class = "label-primary"
     when 71..200 then @rating_class = "label-success"
     end
     output = ''
